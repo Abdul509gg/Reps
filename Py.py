51748ff75e4d7427561a456970ed238956891813
Abdul509gg <habizak06@gmail.com>
2024-08-09 17:53:22 +0100

    Tax.py

--- Py.py
+++ Py.py
@@ -1,2 +1,11 @@
-print ("hello 🌎 world's")
-print ("6677")
+
+def calculate_simple_interest(principal, rate, time):
+    simple_interest = (principal * rate * time) / 100
+    return simple_interest
+
+principal = float(input("500"))
+rate = float(input("10"))
+time = float(input("3"))
+
+simple_interest = calculate_simple_interest(principal, rate, time)
+print(" simple interest ")
